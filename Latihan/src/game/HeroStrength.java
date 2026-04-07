c47329c5c23788812ac81b172d875daace15712c
imanseng <imandwisatrio99@gmail.com>
2026-04-07 16:32:48 +0700

    kirim soal latihan nomor 4

--- Latihan/src/game/HeroStrength.java
+++ Latihan/src/game/HeroStrength.java
@@ -6,4 +6,17 @@ public class HeroStrength extends Hero {
     public HeroStrength(String name, double health, double attackPower) {
         super(name, health, attackPower);
     }
+
+    @Override
+    public void display() {
+        super.display();
+        System.out.println("Tipe: " + tipe);
+    }
+
+    @Override
+    public void terimaSerangan(double damage) {
+        this.health -= damage * 0.5;
+        System.out.println("Serangan diterima " + damage);
+        System.out.println("Health berubah menjadi " + this.health);
+    }
 }
