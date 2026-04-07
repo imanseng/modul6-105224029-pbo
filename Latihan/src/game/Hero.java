bc23067b1344d8cd08725d1f7168e0bed2a0293a
imanseng <imandwisatrio99@gmail.com>
2026-04-07 16:21:44 +0700

    kirim soal 1 latihan

---
+++ Latihan/src/game/Hero.java
@@ -0,0 +1,13 @@
+package game;
+
+public class Hero {
+    String name;
+    double health;
+    double attackPower;
+
+    public Hero(String name, double health, double attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+}
