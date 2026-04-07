b19d14f1fb8798e195788caa5b5447274e83a8f1
imanseng <imandwisatrio99@gmail.com>
2026-04-07 16:29:09 +0700

    kirim latihan soal no 3

---
+++ Latihan/src/game/HeroStrength.java
@@ -0,0 +1,9 @@
+package game;
+
+public class HeroStrength extends Hero {
+    String tipe = "Strength";
+
+    public HeroStrength(String name, double health, double attackPower) {
+        super(name, health, attackPower);
+    }
+}
