258d6c892adbe2854237d1c97b6e930efa9d5591
imanseng <imandwisatrio99@gmail.com>
2026-04-07 16:25:20 +0700

    kirim latihan soal nomor 2

--- Latihan/src/game/Hero.java
+++ Latihan/src/game/Hero.java
@@ -10,4 +10,21 @@ public class Hero {
         this.health = health;
         this.attackPower = attackPower;
     }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Health: " + health);
+        System.out.println("Attack Power: " + attackPower);
+    }
+
+    public void berlatih() {
+        this.attackPower += 10;
+        System.out.println("Attack Power berubah menjadi " + this.attackPower);
+    }
+
+    public void terimaSerangan(double damage) {
+        this.health -= damage;
+        System.out.println("Serangan diterima " + damage);
+        System.out.println("Health berubah menjadi " + this.health);
+    }
 }
